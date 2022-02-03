d806650610cbaf866fe6516773b80d4515f3e154
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-02-03 10:07:20 -0500

    Add admin categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count(:all)
     @category_count = Category.count(:all)
+    @categories_all = Category.all
   end
 end
